2b607281f8ec4260418d1f33ed5818916339b3e4
wave <947142188@qq.com>
2017-06-22 21:08:03 +0800

    增加短信

--- app/service/veCode.js
+++ app/service/veCode.js
@@ -3,7 +3,29 @@ module.exports = app => {
     // 插入验证码
     async insert(mobile, code) {
       const result = await this.app.mysql.insert('data_sms', { sms_mobile: mobile, sms_type: 0, sms_content: code, sms_create_time: this.app.mysql.literals.now });
-      return result.affectedRows === 1;
+
+      const content = '注册用户-验证码：' + code;
+      const sdkurl = 'http://sdk.entinfo.cn:8061/webservice.asmx/mdsmssend?sn=SDK-ZZB-010-00025&pwd=93C8CF517DC35B021D7B50ADCA564850&mobile=' + mobile + '&content=' + content + '【速米科技】&ext=&stime=&rrid=&msgfmt=';
+      const url = this.url_encode(sdkurl);
+      const result2 = await this.app.curl(url, {
+        method: 'GET',
+        // dataType: 'json',
+      });
+      if (result2 && result) {
+        return result.affectedRows === 1;
+      }
+      return 0;
+    }
+
+    url_encode(url) {
+      url = encodeURIComponent(url);
+      url = url.replace(/\%3A/g, ":");
+      url = url.replace(/\%2F/g, "/");
+      url = url.replace(/\%3F/g, "?");
+      url = url.replace(/\%3D/g, "=");
+      url = url.replace(/\%26/g, "&");
+
+      return url;
     }
 
     // 更新验证码状态
